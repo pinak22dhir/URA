f10d27e72fc878aa205b0bff108a2a863f2bed5c
DiyaGoyal0212 <goyal.diya0212@gmail.com>
2024-11-28 00:18:58 +0530

    navbar

--- mediconnectfrontend/Medicine/client/src/components/Navbar.jsx
+++ mediconnectfrontend/Medicine/client/src/components/Navbar.jsx
@@ -27,10 +27,7 @@ const Navbar = () => {
                 <p>Collection</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
-            {/* <NavLink to="/about" className="flex flex-col items-center gap-1">
-                <p>About</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-            </NavLink> */}
+            
             <NavLink to="/contact" className="flex flex-col items-center gap-1">
                 <p>Contact</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
